b20aadc0ca0892fb9496ab59e0efa291b58dd41c
Jangbe <jangebe91@gmail.com>
2021-09-23 20:42:33 +0700

    fix register pricing

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,6 +19,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var string[]
      */
     protected $fillable = [
+        'price',
         'name',
         'email',
         'phone',
